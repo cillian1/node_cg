85fcce9192b50a6e9a1009460dfa258032036677
cillian1 <cilliangreene@live.ie>
2019-02-06 11:39:11 +0000

    Added the index and JS files

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + 'index.html');
+});
+
+app.get('/index',(req,res)=>{
+  res.sendFile(__dirname + '/index.html');
+});
+app.listen(3016);
+console.log("Listening to port 3016");
