d5e89d3630a262876397e028673f610762c3964a
cillian1 <cilliangreene@live.ie>
2019-02-06 11:58:33 +0000

    changed color of text

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
-
-
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + '/index.html');
 });
-
 app.get('/index',(req,res)=>{
   res.sendFile(__dirname + '/index.html');
 });
